bfcc2844f9107e18eff486ad8c8a99a207fbccb1
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2022-03-09 15:17:27 +0200

    Update mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include "mat.hpp"
 
 using namespace std;
 
@@ -7,20 +8,8 @@ namespace ariel{
 
 	
 	//a - collum, b - row, c - char1, d - char2.
-    	string mat(int a,int b,char c,char d){
-        //check odd number
-	if(a%2==0 || b%2==0){
-            throw runtime_error("wrong input - number needs to be odd");
-        }
-	//check if the number is smaller then 0
-	if(a<0 || b<0){
-            throw runtime_error("worng input - number needs to be grather than 0");
-        }
-	
-
-	//str to built the matrix
-        string str;
-    	//for the matrix
-    return str;
+    string mat(int a,int b,char c,char d){
+       string s = "x";
+       return s;
     }
 }
